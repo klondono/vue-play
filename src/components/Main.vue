9a4ed0fa7f79bbc294d3cf095ec51b474c6caf15
klondono <klondono1016@gmail.com>
2017-12-17 21:19:29 -0500

    Initial Commit

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    {{key}}
+        <proclinks></proclinks>
+    </div>
+</template>
+
+<script>
+import proclinks from './ProcLinks.vue';
+
+    export default {
+        components: {
+            proclinks
+        },
+       data() {
+           return {
+               key: "This is main js where we will store some info.",
+               permissions: {Modify: true, Approve: true, Discard: false, Remodify: false, View: true}
+           }
+       }, 
+       methods: {
+           name() {
+               
+           }
+       },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProcLinks.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Action Status</th>
+                    <th>Id</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item) of journalEntries" v-bind:key="item.TxnId">
+                    <td>{{item.ActionStatus}}</td>
+                    <td>{{item.TxnId}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+    export default {
+       data() {
+           const self = this;
+           return {
+               journalEntries: [{ActionStatus: "Await Approval", TxnId: 1},{ActionStatus: "Rejected", TxnId: 2}],
+               dataFromParent: self.$parent.permissions
+           }
+       }, 
+       methods: {
+           name() {
+               
+           }
+       },
+    }
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+}
+table {  
+    color: #333;
+    font-family: Helvetica, Arial, sans-serif;
+    width: 640px; 
+    border-collapse: 
+    collapse; border-spacing: 0; 
+}
+
+td, th {  
+    border: 1px solid transparent; /* No more visible border */
+    height: 30px; 
+    transition: all 0.3s;  /* Simple transition for hover effect */
+}
+
+th {  
+    background: #DFDFDF;  /* Darken header a bit */
+    font-weight: bold;
+}
+
+td {  
+    background: #FAFAFA;
+    text-align: center;
+}
+
+/* Cells in even rows (2,4,6...) are one color */        
+tr:nth-child(even) td { background: #F1F1F1; }   
+
+/* Cells in odd rows (1,3,5...) are another (excludes header cells)  */        
+tr:nth-child(odd) td { background: #FEFEFE; }  
+
+tr td:hover { background: #666; color: #FFF; }  
+/* Hover cell effect! */
+</style>
