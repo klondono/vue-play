547be3a52cd3b2b450cf7be42d02d00252b70b60
klondono <klondono1016@gmail.com>
2017-12-25 16:49:34 -0500

    Added VTable component

---
+++ src/components/VTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <Vuetable
+        api-url="apiURL"
+        :fields="fields"
+        ></Vuetable>
+    </div>
+</template>
+
+<script>
+import {Vuetable} from './node_modules/vuetable-2/src';
+
+    export default {
+        components: {
+            Vuetable
+        },
+        props: {
+            apiURL: {
+                type: String,
+                required: true
+            },
+            fields: {
+                type: Array,
+                required: true
+            }
+        },
+       data() {
+           return {
+           }
+       }, 
+       methods: {
+       },
+    }
+</script>
+
+<style scoped>
+
+</style>
