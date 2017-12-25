8cf59a2e104cdafbdae01f8231c02abd6c54e394
klondono <klondono1016@gmail.com>
2017-12-25 16:39:16 -0500

    Added vuetable component

---
+++ src/components/Vuetable.vue
@@ -0,0 +1,29 @@
+Vue.use(Vuetable);
+new Vue({
+  el: '#app',
+  components:{
+   'vuetable-pagination': Vuetable.VuetablePagination
+  },
+  data: {
+  fields: ['name', 'email','birthdate','nickname','gender','__slot:actions']
+  },
+  computed:{
+  /*httpOptions(){
+    return {headers: {'Authorization': "my-token"}} //table props -> :http-options="httpOptions"
+  },*/
+ },
+ methods: {
+    onPaginationData (paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData)
+    },
+    onChangePage (page) {
+      this.$refs.vuetable.changePage(page)
+    },
+    editRow(rowData){
+      alert("You clicked edit on"+ JSON.stringify(rowData))
+    },
+    deleteRow(rowData){
+      alert("You clicked delete on"+ JSON.stringify(rowData))
+    }
+  }
+})
